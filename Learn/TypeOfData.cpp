809fe6c445220f8be2d7b9796a16968804b91237
LemonMeng <125562592+159Tionfox@users.noreply.github.com>
2023-05-26 08:14:58 +0800

    Add files via upload

---
+++ Learn/TypeOfData.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	// C/C++
+	short a = 12345;
+	int b = 1234567891;
+	//long [windows 8 byte]/[Linux 4 byte]
+	long long c = 123456789123456789;
+	
+	float d = 1.2f;
+	double e = 1.2;
+	
+	bool f = true;//false
+	
+	char g = 'A'; //ASCII
+	
+	char h[] = "abcdefg";
+	
+	// C++
+	std::string i = "abcdefg";
+}
