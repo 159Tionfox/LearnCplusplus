13aa279e5c16a13a33ad9392d797a5a9e7611acd
LemonMeng <125562592+159Tionfox@users.noreply.github.com>
2023-05-26 08:33:44 +0800

    Update TypeOfData.cpp

--- Learn/TypeOfData.cpp
+++ Learn/TypeOfData.cpp
@@ -6,6 +6,7 @@ int main()
 	short a = 12345;
 	int b = 1234567891;
 	//long [windows 4 byte]/[Linux 8 byte]
+	std::cout<<sizeof(long)<<std::endl;	//Output long byte
 	long long c = 123456789123456789;
 	
 	float d = 1.2f;
